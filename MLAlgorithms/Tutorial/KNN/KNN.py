637fc5f00ef33479459d094cb350edc90a1e728b
Augusto Hagemeier <augustolhagemeier@gmail.com>
2025-08-26 13:12:51 -0300

    Feito o LinearRegression

--- MLAlgorithms/Tutorial/KNN/KNN.py
+++ MLAlgorithms/Tutorial/KNN/KNN.py
@@ -1,4 +1,5 @@
 import numpy as np
+from collections import Counter
 
 def euclidean_distance(x1, x2):
   return np.sqrt(np.sum((x1 - x2) ** 2))
@@ -15,12 +16,16 @@ class KNN:
 
   def predict(self, x):
     predictions = [self._predict(xs) for xs in x]
+    return predictions
 
   def _predict(self, x):
     #compute distances 
     distances = [euclidean_distance(x,x_train) for x_train in self.x_train]
 
     #get the closest k 
-    np.argsort(distances)[:self.k]
+    k_indices = np.argsort(distances)[:self.k]
+    k_nearest_labes = [self.y_train[i] for i in k_indices]
 
     #majority vote
+    most_common = Counter(k_nearest_labes).most_common()
+    return most_common[0][0]
